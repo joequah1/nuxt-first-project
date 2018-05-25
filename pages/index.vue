28b8b98d5a8b111c9d52827059b822b65c8cf3d7
Ninjoe <joequah1@gmail.com>
2018-05-25 22:45:35 +0800

    setup

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+<section class="section is-large">
+    <div class="container has-text-centered">
+        <h1 class="title">Hi, I'm Chimezie Enyinnaya</h1>
+        <h2 class="subtitle">a Software Developer based in Lagos, Nigeria.</h2>
+    </div>
+</section>
+</template>
+<script>
+import AppLogo from '~/components/AppLogo.vue'
+
+export default {
+  components: {
+	AppLogo
+  }
+}
+</script>
